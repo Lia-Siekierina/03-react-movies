cd42b7d5a012649ae13a197176b0451f4d062cdd
Lia <natalimail911@gmail.com>
2026-09-10 20:51:36 +0300

    main

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -11,12 +11,13 @@ const MovieGrid = ({ movies, onSelect }: MovieGridProps) => {
     <ul className={css.grid}>
       {movies.map((movie) => (
         <li key={movie.id}>
-          <div className={css.card} onClick={() => onSelect(movie)}>
+          <div className={css.card}>
             <img
               className={css.image}
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
               loading="lazy"
+              onClick={() => onSelect(movie)}
             />
             <h2 className={css.title}>{movie.title}</h2>
           </div>
